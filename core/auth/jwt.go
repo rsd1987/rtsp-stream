b7f5a593ca9c7d0f33733219ea5067b763eda0c9
rsd1987 <rsd1987@qq.com>
2019-04-02 14:37:28 +0800

    Update jwt.go

--- core/auth/jwt.go
+++ core/auth/jwt.go
@@ -6,7 +6,7 @@ import (
 	"io/ioutil"
 	"strings"
 
-	"github.com/Roverr/rtsp-stream/core/config"
+	"github.com/rsd1987/rtsp-stream/core/config"
 	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/sirupsen/logrus"
 )
