94dacd876c33d783e3ec0e2e45b6b974f70fe35c
rsd1987 <rsd1987@qq.com>
2019-04-02 14:37:54 +0800

    Update jwt_test.go

--- core/auth/jwt_test.go
+++ core/auth/jwt_test.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/Roverr/rtsp-stream/core/config"
+	"github.com/rsd1987/rtsp-stream/core/config"
 	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/stretchr/testify/assert"
 )
